2fa95f74ffcd734c14d50173ad9c765589f1861c
paparomeo1969 <github@paparomeo.uk>
2017-10-11 08:31:13 +0100

    Hyperdiction Detector
    Not yet integrated with sheets

--- load.py
+++ load.py
@@ -98,6 +98,16 @@ def findNearest(jumpsystem,list):
 	
 	return nearest,n,list[nearest]["instructions"],list[nearest]["visits"],list[nearest]["x"],list[nearest]["y"],list[nearest]["z"]
 	
+def detect_hyperdiction(cmdr,timestamp,endjump,startjump=None):
+	if startjump == None:
+		print "No startjump event: Is that even possible"
+	if startjump == endjump:
+		print "Hyperdiction detected."
+		# log it to a spreadheet
+		# show it in the status with a pre-filled link to the Canonn form.
+	
+	
+		
 	
 # Detect journal events
 def journal_entry(cmdr, system, station, entry):
@@ -109,6 +119,7 @@ def journal_entry(cmdr, system, station, entry):
 		
 	  
 	if entry['event'] == 'USSDrop':
+		#set some variables for logging when we exit supercruise
 		this.uss=True
 		this.usstype=entry['USSType']
 		this.usslocal=entry['USSType_Localised']
@@ -121,28 +132,42 @@ def journal_entry(cmdr, system, station, entry):
 			this.uss=False
 			
 			this.status['text']="Logging: "+this.usslocal
-			url = "https://docs.google.com/forms/d/e/1FAIpQLScVk2LW6EkIW3hL8EhuLVI5j7jQ1ZmsYCLRxgCZlpHiN8JdcA/formResponse?usp=pp_url&entry.582675236="+quote_plus(entry['StarSystem'])+"&entry.413701316="+quote_plus(entry['Body'])+"&entry.218543806="+quote_plus(this.usstype)+"&entry.455413428="+quote_plus(this.usslocal)+"&entry.790504343="+quote_plus(this.threat)+"&submit=Submit"
+			#url  = "https://docs.google.com/forms/d/e/1FAIpQLScVk2LW6EkIW3hL8EhuLVI5j7jQ1ZmsYCLRxgCZlpHiN8JdcA/formResponse?usp=pp_url&entry.106150081="+cmdr+"&entry.582675236="+quote_plus(entry['StarSystem'])+"&entry.413701316="+quote_plus(entry['Body'])+"&entry.218543806="+quote_plus(this.usstype)+"&entry.455413428=+"quote_plus(this.usslocal)+"&entry.790504343="+quote_plus(this.threat)+"&submit=Submit"
+			url = "https://docs.google.com/forms/d/e/1FAIpQLScVk2LW6EkIW3hL8EhuLVI5j7jQ1ZmsYCLRxgCZlpHiN8JdcA/formResponse?usp=pp_url&entry.106150081="+cmdr+"&entry.582675236="+quote_plus(entry['StarSystem'])+"&entry.413701316="+quote_plus(entry['Body'])+"&entry.218543806="+quote_plus(this.usstype)+"&entry.455413428="+quote_plus(this.usslocal)+"&entry.790504343="+quote_plus(this.threat)+"&submit=Submit"
 			#print url
 			r = requests.get(url)	
 			print r
 		
 	if entry['event'] == 'StartJump' and entry['JumpType'] == 'Hyperspace':
-			#When we start a jump we are leaving the system so we can log our jump
-			try:	
-				#we might have not captured the arrival because we were offline
-				this.arrived
-			except:
-				this.arrived=entry["timestamp"]
 			
-			url = "https://docs.google.com/forms/d/e/1FAIpQLScmM7IuJAla_9LflBf-Bi7aNsIhbNkuh_3g6_Z2PL87zMzXGg/formResponse?usp=pp_url&entry.1836345870="+this.arrived+"&entry.25192571="+entry["timestamp"]+"&entry.424221764="+cmdr+"&entry.799655481="+system			
-			r = requests.get(url)	
-			print r
-			print "Jump started: " +cmdr+"  "+system
+		#When we start a jump we are leaving the system so we can log our jump
+		try:	
+			#we might have not captured the arrival because we were offline
+			this.arrived
+		except:
+			this.arrived=entry["timestamp"]
 			
-			
+		this.startjump=system
+		
+		try:	
+			#If the system we are jumping out of is one of the nearest then 
+			# we need to log it otherwise dont both
+			if this.nearest == system:
+				try:
+					url = "https://docs.google.com/forms/d/e/1FAIpQLScmM7IuJAla_9LflBf-Bi7aNsIhbNkuh_3g6_Z2PL87zMzXGg/formResponse?usp=pp_url&entry.1836345870="+this.arrived+"&entry.25192571="+entry["timestamp"]+"&entry.424221764="+cmdr+"&entry.799655481="+system			
+					r = requests.get(url)	
+					print r
+					print "Jump started: " +cmdr+"  "+system
+				except:
+					print "error sending "+url 
+		except:	
 			print entry
+			
+			
 	
 	if entry['event'] == 'FSDJump':
+			detect_hyperdiction(cmdr,entry["timestamp"],entry["StarSystem"],this.startjump)
+					
 			#set the arrival time for locgging
 			this.arrived=entry["timestamp"]
 			#we have coordinates so we can find the nearest system
